0ed0d851c1dc1df68ef4af9b6e1502c17443ee68
hyorward <arturhyorward@gmail.com>
2026-04-16 17:14:00 +0300

    feat(chat): enforce maximum length for server and channel names

--- sql/schema/002_servers.sql
+++ sql/schema/002_servers.sql
@@ -7,3 +7,4 @@ CREATE TABLE servers (
 );
 -- +goose Down
 DROP TABLE IF EXISTS servers;
+
--- sql/schema/004_channels.sql
+++ sql/schema/004_channels.sql
@@ -2,7 +2,7 @@
 CREATE TABLE channels (
     id bigserial PRIMARY KEY,
     server_id bigserial REFERENCES servers(id) ON DELETE CASCADE,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL CHECK (char_length(name) <= 16),
     type TEXT DEFAULT 'text',
     position INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT now()
