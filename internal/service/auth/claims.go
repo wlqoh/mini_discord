95c9484f96d1123cf6c3aee33cb0a31545cd510b
wlqoh <wylqoh@gmail.com>
2026-05-15 17:46:29 +0000

    simplify JWT creation by removing unnecessary user details

--- internal/service/auth/claims.go
+++ internal/service/auth/claims.go
@@ -9,27 +9,20 @@ import (
 )
 
 type UserClaims struct {
-	UserID    int    `json:"user_id"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	UserID int `json:"user_id"`
 	jwt.RegisteredClaims
 }
 
-func NewUserClaims(userID int, email, firstName, lastName string, duration time.Duration) (*UserClaims, error) {
+func NewUserClaims(userID int, duration time.Duration) (*UserClaims, error) {
 	tokenID, err := uuid.NewRandom()
 	if err != nil {
 		return nil, fmt.Errorf("new user claims: %w", err)
 	}
 
 	return &UserClaims{
-		Email:     email,
-		UserID:    userID,
-		FirstName: firstName,
-		LastName:  lastName,
+		UserID: userID,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ID:        tokenID.String(),
-			Subject:   email,
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
 		},
